7c4c839f0de2121d54dba00f91ec68a4f77515a5
Caerisse <avellanedaandres.08@gmail.com>
2020-08-12 19:21:53 -0300

    Added models to admin site

--- app_web/admin.py
+++ app_web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_main.models import *
+
+admin.site.register(ICerebroUser)
+admin.site.register(InstaUser)
+admin.site.register(FollowRelation)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(BotCookies)
+admin.site.register(BotFollowed)
+admin.site.register(BotBlacklist)
+admin.site.register(BotSettings)
+admin.site.register(BotScheduledPost)
